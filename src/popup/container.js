5bcc91b9c5c331a49c505894e0177a95288899ab
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-06-11 12:11:34 +0700

    Implement adding of tags on unindexed pages
    Migrate IndexDropdown container to TS
    
    - handles:
      - tag adding via popup or overview
      - domain/tag suggestions
    - plus some JSDoc types because TS is very painful with existing react components written in JS (doesn't seem to use propTypes info)
    
    Implement adding of tags on unindexed pages
    
    - previously the tags menu was greyed out for unindexed pages
    - now we are allowing these pages to get indexed on demand, via the browser tab data, if a user tries to add tags to them
    
    Set up optimistic UI for new popup tags indexing
    
    - UI should not wait for the page to get processed and indexed now; just attach the tag immediately from user POV

--- src/popup/container.js
+++ src/popup/container.js
@@ -139,7 +139,7 @@ class PopupContainer extends Component {
     }
 
     get isTagBtnDisabled() {
-        return !this.state.isLoggable || this.state.page == null
+        return !this.state.isLoggable
     }
 
     get bookmarkBtnState() {
@@ -367,6 +367,7 @@ class PopupContainer extends Component {
             return (
                 <IndexDropdown
                     url={this.state.url}
+                    tabId={this.state.tabID}
                     initFilters={this.pageTags}
                     source="tag"
                 />
