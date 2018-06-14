d1d1eecdafdcfc963242299521fefd99f0f4758d
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-06-14 12:01:35 +0700

    Update overview bookmarking state changes to be optimistic
    - now that overview bookmarking can trigger an XHR (if page is a stub), it can take a while
    - no need to wait for that; just perform the state change immediately and revert if any errors occur

--- src/overview/actions.js
+++ src/overview/actions.js
@@ -227,18 +227,17 @@ export const toggleBookmark = (url, index) => async (dispatch, getState) => {
             : 'Create result bookmark',
     })
 
-    try {
-        // Either perform adding or removal of bookmark if
-        if (hasBookmark) {
-            await removeBookmarkByUrl({ url })
-        } else {
-            await createBookmarkByUrl({ url })
-        }
-    } catch (error) {
-        dispatch(changeHasBookmark(index)) // Reset UI state in case of error
-    } finally {
-        updateLastActive() // Consider user active (analytics)
+    // Reset UI state in case of error
+    const errHandler = err => dispatch(changeHasBookmark(index))
+
+    // Either perform adding or removal of bookmark; do not wait for ops to complete
+    if (hasBookmark) {
+        removeBookmarkByUrl({ url }).catch(errHandler)
+    } else {
+        createBookmarkByUrl({ url }).catch(errHandler)
     }
+
+    updateLastActive() // Consider user active (analytics)
 }
 
 export const showTags = index => (dispatch, getState) => {
